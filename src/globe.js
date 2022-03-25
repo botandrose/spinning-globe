c1b7563a89783eb315bbe78ec9b1dfb5f9b4ed70
Micah Geisel <micah@botandrose.com>
2022-03-24 18:00:45 -0700

    extract ImageLoader.

--- src/globe.js
+++ src/globe.js
@@ -9,53 +9,10 @@ const OrbitControls = BuildOrbitControls(THREE);
 import BuildTrackballControls from "./TrackballControls.js";
 const TrackballControls = BuildTrackballControls(THREE);
 
+import BuildImageLoader from "./ImageLoader.js";
+const ImageLoader = BuildImageLoader(THREE);
+
 import Detector from "./Detector.js";
-
-class ImageLoader {
-  constructor(manager) {
-    this.manager = ( manager !== undefined ) ? manager : THREE.DefaultLoadingManager;
-  }
-
-  load ( url, onLoad, onProgress, onError ) {
-    const scope = this;
-
-    const image = document.createElementNS('http://www.w3.org/1999/xhtml', 'img');
-    image.onload = function () {
-      image.onload = null;
-      URL.revokeObjectURL( image.src );
-      if ( onLoad ) onLoad( image );
-      scope.manager.itemEnd( url );
-    };
-    image.onerror = onError;
-
-    const loader = new THREE.FileLoader();
-    loader.setPath( this.path );
-    loader.setResponseType( 'blob' );
-    loader.setWithCredentials( this.withCredentials );
-    loader.load( url, ( blob ) => {
-      image.src = URL.createObjectURL( blob );
-    }, onProgress, onError );
-
-    scope.manager.itemStart( url );
-
-    return image;
-  }
-
-  setCrossOrigin ( value ) {
-    this.crossOrigin = value;
-    return this;
-  }
-
-  setWithCredentials ( value ) {
-    this.withCredentials = value;
-    return this;
-  }
-
-  setPath ( value ) {
-    this.path = value;
-    return this;
-  }
-};
 
 export default function(container, sourceSet, background, specular, fullscreen, inside) {
     // Get current globe slug from URL
