5300301d3d24bd8d82841a3315b6d7c6849465b2
Micah Geisel <micah@botandrose.com>
2022-03-25 14:58:23 -0600

    try to get something working with loading bar.

--- src/SpinningGlobe.js
+++ src/SpinningGlobe.js
@@ -9,7 +9,62 @@ export class SpinningGlobe extends LitElement {
         color: var(--spinning-globe-text-color, #000);
       }
       #webgl {
+        position: relative;
         height: 100%;
+        // canvas
+        //   height: auto !important
+        //   max-width: 100%
+      }
+      #element {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        z-index: 1;
+        position: static !important;
+      }
+
+      .loading-screen {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: calc(50% - 100px);
+        right: 0;
+        width: 200px;
+        height: 100%;
+        text-align: center;
+        text-transform: uppercase;
+        letter-spacing: 0.15em;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        z-index: 1;
+      }
+
+      .globe .loading-screen {
+        height: 100vh;
+      }
+
+      .loading-screen__loader {
+        border-radius: 5px,
+        height: 10px,
+        display: inline-block,
+        font-size: 10px,
+        position: relative,
+        text-indent: -9999em,
+        background: #76A3A3,
+        max-width: 95%,
+      }
+
+      .loading-screen__loader:before {
+        content: "";
+        position: absolute;
+        z-index: 1;
+        border: 1px solid rgba(#fff, 0.4);
+        width: 199px;
+        height: calc(100% - 1px);
+        left: 0;
+        top: 0;
+        border-radius: 5px;
       }
     `;
   }
