d3793d803917b64d9b4c7fc0e43ec34bd1045dac
Michael Gubitosa <gubs@botandrose.com>
2022-03-26 17:30:20 -0700

    Get loading styles working

--- src/SpinningGlobe.js
+++ src/SpinningGlobe.js
@@ -7,7 +7,8 @@ export class SpinningGlobe extends LitElement {
   static get styles() {
     return css`
       :host {
-        display: inline-block;
+        display: block;
+        position: relative;
         color: var(--spinning-globe-text-color, #000);
       }
       #webgl {
@@ -17,56 +18,52 @@ export class SpinningGlobe extends LitElement {
         //   height: auto !important
         //   max-width: 100%
       }
-      #element {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        z-index: 1;
-        position: static !important;
-      }
 
       .loading-screen {
         position: absolute;
-        top: 0;
-        bottom: 0;
+        top: calc(50% - 5px);
         left: calc(50% - 100px);
-        right: 0;
         width: 200px;
-        height: 100%;
+        height: 10px;
         text-align: center;
         text-transform: uppercase;
-        letter-spacing: 0.15em;
         display: flex;
         justify-content: flex-start;
         align-items: center;
         z-index: 1;
-      }
-
-      .globe .loading-screen {
-        height: 100vh;
+        overflow: hidden;
+        box-shadow: rgba(0, 0, 0, 0.25) 0 2px 10px;
+        box-sizing: border-box;
       }
 
       .loading-screen__loader {
-        border-radius: 5px,
-        height: 10px,
-        display: inline-block,
-        font-size: 10px,
-        position: relative,
-        text-indent: -9999em,
-        background: #76A3A3,
-        max-width: 95%,
+        border-top-left-radius: 5px;
+        border-bottom-left-radius: 5px;
+        height: 10px;
+        display: inline-block;
+        font-size: 10px;
+        position: relative;
+        text-indent: -9999em;
+        background: linear-gradient(
+          rgba(255, 255, 255, 0.25),
+          rgba(255, 255, 255, 0.1)
+        );
+        color: transparent;
+        max-width: 95%;
+        box-sizing: border-box;
       }
 
       .loading-screen__loader:before {
-        content: "";
+        content: '';
         position: absolute;
         z-index: 1;
-        border: 1px solid rgba(#fff, 0.4);
+        border: 1px solid rgba(255, 255, 255, 0.15);
         width: 199px;
-        height: calc(100% - 1px);
+        height: 100%;
         left: 0;
         top: 0;
         border-radius: 5px;
+        box-sizing: border-box;
       }
     `;
   }
@@ -75,9 +72,9 @@ export class SpinningGlobe extends LitElement {
     srcset: { type: String },
     background: { type: String },
     specular: { type: Number },
-    inside: { converter: value => ["inside", "true"].includes(value) },
+    inside: { converter: value => ['inside', 'true'].includes(value) },
     _loading: { state: true },
-  }
+  };
 
   constructor() {
     super();
@@ -87,13 +84,14 @@ export class SpinningGlobe extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
-    this.sourceSet = this.__parseSrcset()
+    this.sourceSet = this.__parseSrcset();
   }
 
   __parseSrcset() {
-    return this.srcset.split(",")
+    return this.srcset
+      .split(',')
       .map(line => line.match(/([^ ]+) ([^, ]+)/)?.[1])
-      .filter(a => a)
+      .filter(a => a);
   }
 
   firstUpdated() {
@@ -103,13 +101,18 @@ export class SpinningGlobe extends LitElement {
 
   render() {
     return html`
-      <div id="element">
-        ${this._loading < 100 ? html`
-          <div class="loading-screen">
-            <div class="loading-screen__loader" style="${styleMap({ width: this._loading + '%' })}">Loading...</div>
-          </div>
-        ` : ''}
-      </div>
+      ${this._loading < 100
+        ? html`
+            <div class="loading-screen">
+              <div
+                class="loading-screen__loader"
+                style="${styleMap({ width: this._loading + '%' })}"
+              >
+                Loading...
+              </div>
+            </div>
+          `
+        : ''}
       <div id="webgl"></div>
     `;
   }
