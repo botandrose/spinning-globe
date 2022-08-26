17e0934bcc6e57eef792dcf3bb084a67265a4eda
Micah Geisel <micah@botandrose.com>
2022-08-26 08:35:39 +0200

    Revert "force ipad to 8k res in fullscreen mode."
    This reverts commit 1ad05c68d0213643cb64167b02217733a92c72c9.

--- src/SpinningGlobe.js
+++ src/SpinningGlobe.js
@@ -59,7 +59,6 @@ export class SpinningGlobe extends LitElement {
       /iPad|iPhone|iPod/.test(navigator.platform) ||
       (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
     const isFirefox = /Firefox/.test(navigator.userAgent);
-    const isIPad = /iPad/.test(navigator.platform);
 
     const width = this.clientWidth;
     const fullscreen = width > 1000;
@@ -67,7 +66,7 @@ export class SpinningGlobe extends LitElement {
       fullscreen && !isMobile && !isPretendingToBeDesktop
         ? "16k"
         : "4k";
-    if (fullscreen && (isFirefox || isIPad)) {
+    if (fullscreen && isFirefox) {
       density = "8k";
     }
     return density;
